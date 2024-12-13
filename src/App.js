1e065e609ddcfd65bb003b8b8134ebc00ce97f22
ngupta0843 <ngupta0843@gmail.com>
2024-12-12 23:51:17 -0800

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="Nikhil Gupta's Personal Website">
+      <header className="App-header">
+        Nikhil Gupta
+      </header>
+    </div>
+  );
+}
+
+export default App;
